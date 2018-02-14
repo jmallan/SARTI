c79e4154fa3ea8d76a48ea46f39556aa30d2c902
curs2018 <curs2018@BI101-12.epsevg.upc.es>
2018-02-14 11:01:14 +0100

    update

--- 1.2/js/eje14.js
+++ 1.2/js/eje14.js
@@ -12,11 +12,14 @@ function modifica(){
     var ver = this.className;   //capto la class de l'enllaç
     if (ver == "visible"){      //comprovo si es visible o no i actuo acord a ell
     //fer amb previousSibling per identificar el germà anterior.
-    this.previousSibling.previousSibling.style.display="none";
+    this.previousSibling.previousSibling.style.display="none"; // això es podria escriure al full css, al canviar la classe canviarà l'estil
     this.className="oculto";
+    this.innerHTML = "Mostrar contenido";
     }
     else {
+        //plantejat com el cas anterior però a l'inversa.
     this.previousSibling.previousSibling.style.display="block";
     this.className="visible";
+    this.innerHTML = "Ocultar contenido";
     }
 }
--- 1.2/js/js13.js
+++ 1.2/js/js13.js
@@ -3,6 +3,7 @@ window.addEventListener('load', cargaEventos);
 function cargaEventos(){
 	//document.getElementsByTagName("input")[0].addEventListener("click", anade);
 	document.querySelector("input[value='Añadir elemento']").addEventListener("click", anade);
+	document.querySelector("input[value='Eliminar elemento']").addEventListener("click", elimina);
 }
 
 function anade() {
@@ -10,4 +11,17 @@ function anade() {
 	var text = document.createTextNode("New");
 	elLi.appendChild(text);
 	document.getElementById("lista").appendChild(elLi);
+	var numLi = document.getElementsByTagName("li").length-1;
+	if(numLi >= 0){
+		document.querySelector("input[value='Eliminar elemento']").style.display="initial";
+	}
+}
+
+function elimina() {
+	var numLi = document.getElementsByTagName("li").length-1;
+	var removeLi = document.getElementsByTagName("li")[numLi];
+	removeLi.parentNode.removeChild(removeLi);
+	if(numLi <= 0){
+		document.querySelector("input[value='Eliminar elemento']").style.display="none";
+	}
 }
